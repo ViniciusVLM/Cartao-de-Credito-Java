23624535bf2fb2bbd8358a5d60530c055eeba857
Ogahit <thiago2077pere@gmail.com>
2025-06-11 00:52:58 -0300

    atualizado

--- OneDrive/Documentos/NetBeansProjects/CartaoDeCredito/src/main/java/com/mycompany/cartaodecredito/Usuario.java
+++ OneDrive/Documentos/NetBeansProjects/CartaoDeCredito/src/main/java/com/mycompany/cartaodecredito/Usuario.java
@@ -4,27 +4,36 @@ package com.mycompany.cartaodecredito;
 
 
 public class Usuario {
-    private final String nome;
+ private final String nome;
     private final String cpf;
     private final String senha;
-    private final boolean isAdmin;
 
     public Usuario(String nome, String cpf, String senha, boolean isAdmin) {
         this.nome = nome;
         this.cpf = cpf;
         this.senha = senha;
-        this.isAdmin = isAdmin;
     }
-
 
     public String getNome() {
         return nome;
     }
 
-    boolean isAdmin() {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public String getCpf() {
+        return cpf;
     }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void exibirPerfil() {
+        System.out.println("Usuário: " + nome);
+    }
+     boolean isAdmin() {
+        throw new UnsupportedOperationException("Not supported yet.");
+     }       
 }
+
 
 
 
